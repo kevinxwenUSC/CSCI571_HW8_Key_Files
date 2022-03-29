0631a588c4e026c28f313aa046e655fe75c1d463
kevinxwenUSC <97578450+kevinxwenUSC@users.noreply.github.com>
2022-03-29 04:21:46 -0700

    modules being used so far

---
+++ app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; 
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import { TestComponentComponent } from './components/test-component/test-component.component';
+import { WatchListComponentComponent } from './components/watch-list-component/watch-list-component.component';
+import { SearchComponent } from './components/search/search.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TestComponentComponent,
+    WatchListComponentComponent,
+    SearchComponent,
+    PortfolioComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    MatTabsModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
