b73f8e00828d47eadbcaa702e0fedb45deada6a9
kevinxwenUSC <97578450+kevinxwenUSC@users.noreply.github.com>
2022-03-29 23:16:59 -0700

    autocomplete works

--- search.component.ts
+++ search.component.ts
@@ -6,6 +6,8 @@ import { FormsModule } from '@angular/forms';
 import { FormControl } from '@angular/forms';
 import {Observable} from 'rxjs';
 import {map, startWith} from 'rxjs/operators';
+import { debounceTime, tap, switchMap, finalize, distinctUntilChanged, filter } from 'rxjs/operators';
+
 
 
 @Component({
@@ -16,6 +18,16 @@ import {map, startWith} from 'rxjs/operators';
 export class SearchComponent implements OnInit {
   myControl = new FormControl();
   options: string[] = ['One', 'Two', 'Three'];
+
+
+  filteredStocks:any = []
+  stockOptions:any = []
+
+  isLoading = false;
+  errorMsg!: string;
+  minLengthTerm = 2;
+  selectedStock: any = "";
+
   
   tickerAutocomplete = []
   companyResponseData:any = []
@@ -25,13 +37,69 @@ export class SearchComponent implements OnInit {
    
   }
 
+
+  onSelected() {
+    console.log(this.selectedStock);
+    this.selectedStock = this.selectedStock;
+  }
+
+  displayWith(value: any) {
+    return value?.Symbol;
+  }
+
+  clearSelection() {
+    this.selectedStock = "";
+    this.filteredStocks = [];
+  }
+
+
+
+
   ngOnInit(): void {
+    this.myControl.valueChanges
+      .pipe(
+        filter(res => {
+          return res !== null && res.length >= this.minLengthTerm
+        }),
+        distinctUntilChanged(),
+        debounceTime(1000),
+        tap(() => {
+          this.errorMsg = "";
+          this.filteredStocks = [];
+          this.isLoading = true;
+        }),
+        switchMap(value => this.http.get('/autocomplete/' + value)
+          .pipe(
+            finalize(() => {
+              this.isLoading = false
+            }),
+          )
+        )
+      )
+      .subscribe((data) => {
+        //data['result'] == undefined
+        /*if (data == undefined) {
+          this.errorMsg = "no search results";
+          this.filteredStocks = [];
+        } else {
+          this.errorMsg = "";
+          //this.filteredStocks = data['result'];
+          this.filteredStocks = JSON.parse(data);
+        }*/
+        this.errorMsg = "";
+        //this.filteredStocks = data['result'];
+        this.stockOptions= data;
+        this.filteredStocks = this.stockOptions.result
+        console.log(this.stockOptions.count);
+        console.log(this.filteredStocks)
+      });
   }
 
   getData(){
     this.companyTicker = (<HTMLInputElement>document.getElementById('query')).value;
     console.log(this.companyTicker)
     const url ='/search/' + this.companyTicker
+
     this.http.get(url).subscribe((res)=>{
       this.companyResponseData = res
       console.log(this.companyResponseData)
