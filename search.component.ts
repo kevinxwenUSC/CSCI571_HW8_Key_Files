f67b10cd605dbc0bd5290a0ff6c79bdb96d90a32
kevinxwenUSC <97578450+kevinxwenUSC@users.noreply.github.com>
2022-03-31 02:28:12 -0700

    updated to generate info for highcharts

--- search.component.ts
+++ search.component.ts
@@ -8,6 +8,8 @@ import {Observable} from 'rxjs';
 import {map, startWith} from 'rxjs/operators';
 import { debounceTime, tap, switchMap, finalize, distinctUntilChanged, filter } from 'rxjs/operators';
 
+import * as Highcharts from 'highcharts';
+
 
 
 @Component({
@@ -17,9 +19,8 @@ import { debounceTime, tap, switchMap, finalize, distinctUntilChanged, filter } 
 })
 export class SearchComponent implements OnInit {
   myControl = new FormControl();
-  options: string[] = ['One', 'Two', 'Three'];
 
-
+  //stuff for autocomplete
   filteredStocks:any = []
   stockOptions:any = []
 
@@ -28,7 +29,7 @@ export class SearchComponent implements OnInit {
   minLengthTerm = 2;
   selectedStock: any = "";
 
-  
+  //store JSON data from node.js server responses
   companyResponseData:any = []
   companyTicker:string = ''
   percentChangeinParen:string = ''
@@ -43,7 +44,70 @@ export class SearchComponent implements OnInit {
 
   companyEarningData:any = []
 
+  //data for highcharts chart
   companyHistoryData:any = []
+  historyClosePrice:any = []
+  historyHighPrice:any = []
+  historyLowPrice:any = []
+  historyOpenPrice:any = []
+  historyTimeStamp:any = []
+  historyVolume:any = []
+
+  //store converted time from unix to date obj
+  historyNormalTime:any = []
+
+  //store our combined data for highcharts to use
+  ohlc:any = []
+  volume:any = []
+
+
+
+  //highchart settings stuff here
+  highcharts = Highcharts;
+  chartOptions = {   
+      chart: {
+         type: "spline"
+      },
+      title: {
+         text: "Monthly Average Temperature"
+      },
+      subtitle: {
+         text: "Source: WorldClimate.com"
+      },
+      xAxis:{
+         categories:["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+            "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+      },
+      yAxis: {          
+         title:{
+            text:"Temperature °C"
+         } 
+      },
+      tooltip: {
+         valueSuffix:" °C"
+      },
+      series: [
+         {
+            name: 'Tokyo',
+            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2,26.5, 23.3, 18.3, 13.9, 9.6]
+         },
+         {
+            name: 'New York',
+            data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8,24.1, 20.1, 14.1, 8.6, 2.5]
+         },
+         {
+            name: 'Berlin',
+            data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
+         },
+         {
+            name: 'London',
+            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+         }
+      ] as Highcharts.SeriesOptionsType[]
+   };
+
+
+
 
   constructor(private http: HttpClient, public element: ElementRef) {
    
@@ -170,6 +234,36 @@ export class SearchComponent implements OnInit {
     this.http.get(history_url).subscribe((res)=>{
       this.companyHistoryData= res
       console.log(this.companyHistoryData)
+
+      //separate the data to be passed to the chart
+      this.historyClosePrice = this.companyHistoryData.c
+      this.historyHighPrice = this.companyHistoryData.h
+      this.historyLowPrice = this.companyHistoryData.l
+      this.historyOpenPrice = this.companyHistoryData.o
+      this.historyTimeStamp = this.companyHistoryData.t
+      this.historyVolume = this.companyHistoryData.v
+
+      console.log(this.historyClosePrice)
+
+      //convert unix timestamps to normal dates
+      for(let i = 0; i < this.historyTimeStamp.length; i++){
+        //var normalDate = new Date(this.historyTimeStamp[i] * 1000)
+
+        //acount for x1000 offset for UNIX timestamps in Javascript/Typescript
+        var normalDate = this.historyTimeStamp[i] * 1000
+        this.historyNormalTime.push(normalDate)
+      }
+
+      console.log(this.historyTimeStamp)
+      console.log(this.historyNormalTime)
+
+      //fill the ohlc and volume data for highcharts to use
+      for(let i = 0; i < this.historyTimeStamp.length; i++){
+        this.ohlc.push([this.historyNormalTime[i], this.historyOpenPrice[i], this.historyHighPrice[i], this.historyLowPrice[i], this.historyClosePrice[i]]);
+        this.volume.push([this.historyNormalTime[i], this.historyVolume[i]])
+      }
+
+      console.log(this.volume)
 
     })
 
